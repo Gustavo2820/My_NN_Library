3cd9e91507cae9b8e8ffa20cacee836e22c7ce3e
Gustavo Oliveira <gugui2820@gmail.com>
2024-09-07 23:20:15 -0300

    modified:   mynnlib/utils.py

--- mynnlib/utils.py
+++ mynnlib/utils.py
@@ -88,6 +88,67 @@ def save_trains_results(results, filename='train_results.json'):
     with open(filename, 'w') as f:
         json.dump(results, f)
 
-# Fast Fourier Transform
-def fft(func: function):
-    pass
+def is_power_of_2(n):
+    return (n > 0) and (n & (n - 1)) == 0
+
+def next_power_of_2(n):
+    return 1 << (n - 1).bit_length()
+
+def zero_pad(P, length):
+    return np.pad(P, (0, length - len(P)), mode='constant')
+
+def fft(P):
+    # Pad data if necessary
+    n = len(P)
+    if not is_power_of_2(n):
+        new_length = next_power_of_2(n)
+        P = zero_pad(P, new_length)
+        n = new_length
+
+    # FFT implementation
+    if n <= 1:
+        return np.array(P, dtype=np.complex128)
+    
+    omega = np.exp(-2j * np.pi / n * np.arange(n))
+    
+    P_even = P[::2]
+    P_odd = P[1::2]
+    
+    y_even = fft(P_even)
+    y_odd = fft(P_odd)
+    
+    y = np.zeros(n, dtype=np.complex128)
+    
+    for j in range(n // 2):
+        y[j] = y_even[j] + omega[j] * y_odd[j]
+        y[j + n // 2] = y_even[j] - omega[j] * y_odd[j]
+    
+    return y
+
+def inverse_fft(P):
+    # Pad data if necessary
+    n = len(P)
+    if not is_power_of_2(n):
+        new_length = next_power_of_2(n)
+        P = zero_pad(P, new_length)
+        n = new_length
+
+    # Bit-reversal permutation
+    P_reversed = np.empty_like(P, dtype=complex)
+    for i in range(n):
+        i_reversed = int(f'{i:0{int(np.log2(n))}b}'[::-1], 2)
+        P_reversed[i_reversed] = P[i]
+
+    # FFT computation (same as FFT but with reversed signs for twiddle factors)
+    for size in range(2, n + 1, 2):
+        half_size = size // 2
+        twiddle_factors = np.exp(2j * np.pi * np.arange(half_size) / size)
+        
+        for i in range(0, n, size):
+            for j in range(half_size):
+                t = twiddle_factors[j] * P_reversed[i + j + half_size]
+                u = P_reversed[i + j]
+                P_reversed[i + j] = u + t
+                P_reversed[i + j + half_size] = u - t
+
+    return P_reversed / n
